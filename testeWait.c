3e97690929d8a62112fcb8a844c365936204e7b2
luisfelipehsr <luisfelipehsr@gmail.com>
2017-06-30 23:56:42 -0300

    Agora existem funcoes padroes para lidar com o tempo

--- testeWait.c
+++ testeWait.c
@@ -3,10 +3,27 @@
 #include <math.h>
 #include <limits.h>
 
+#define CLOUD 0
+#define SECURITY 1
+#define ENTERTAINMENT 2
+#define CONFORT 3
+#define INTERVAL_SECURITY 50
+#define INTERVAL_ENTERTAINMENT 3000
+#define INTERVAL_CONFORT 2000
+
+/* estrutura que guarda o horario da ultima mensagem de certo tipo */
+typedef struct time_register {
+	long last_entertainment;
+	long last_confort;
+	long last_security;
+} time_register;
+
+/* retorna o tempo atual em ms */
 long get_time () {
 	struct timespec time;
 	long time_sec;
 	long time_ms;
+	time_register tregister;
 
 	if (clock_gettime(CLOCK_REALTIME, &time))
 		return 0;
@@ -17,27 +34,61 @@ long get_time () {
 	return time_ms;
 }
 
-int main() {
-	long time, initial_time;
-	int i;
+/* Constroi um registrador de horarios */
+void buildTimeRegister(time_register *tr, long currentTime) {
+	tr->last_entertainment = currentTime;
+	tr->last_confort = currentTime;
+	tr->last_security = currentTime;
+}
+
+/* Determina se esta no horario de algo de acordo com um intervalo */
+int isTime (int mode, long currentTime, time_register *tr) {
+	if (mode == ENTERTAINMENT &&
+		currentTime - tr->last_entertainment > INTERVAL_ENTERTAINMENT) {
+	    tr->last_entertainment = currentTime;
+		return 1;
+	} else if (mode == CONFORT &&
+			   currentTime - tr->last_confort > INTERVAL_CONFORT) {
+		tr->last_confort = currentTime;
+		return 1;
+	}
+
+	return 0;
+}
+
+/* Envia mensagens se estiver dentro do intervalo especificado */
+void sendMessages (long currentTime, time_register *tr) {
+	if (isTime(ENTERTAINMENT, currentTime, tr))
+		printf("Are you entertained?\n");
 	
-	i = 0;
+	if (isTime(CONFORT, currentTime, tr))
+	    printf("You have a new notification on Facebook\n");
+}
+
+
+int main() {
+	long time, initial_time, tlast_entertain, tlast_confort;
+	int i;
+	time_register tr;
+	
+	// horario inicial
 	time = get_time();
 	if (!time) {
 		printf("ERROR: can't retrive time\n");
 	}
-
-	printf("Current time %ld ms\n", time);
-
+	
 	initial_time = time;
-
-	i = 0;
-	while(i < 5) {
+	buildTimeRegister(&tr, time);
+	time++; // somente para entrar no loop, nao muda a execucao
+	
+	while(time - initial_time < 5000) {
+		
 		time = get_time();
-		if (time - initial_time > 500*i) {
-			printf("Got a 0.5s interval! %ld\n", time);
-			i++;
+		if (!time) {
+			printf("ERROR: can't retrive time\n");
 		}
+
+		sendMessages(time, &tr);
 	}
 
 	return 0;
