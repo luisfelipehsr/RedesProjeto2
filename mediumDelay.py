5c6e9cbd4522258ae4272f813b814b9ddc8d0987
luisfelipehsr <luisfelipehsr@gmail.com>
2017-07-05 22:12:42 -0300

    medidor de delay

---
+++ mediumDelay.py
@@ -0,0 +1,16 @@
+import re
+import sys
+
+file = open(sys.argv[1], "r")
+lines = file.readlines()
+file.close()
+i = 0
+total = 0
+    for line in lines:
+        if re.search(r"Maximum delay", line) is not None:
+            valueList = re.findall(r"[0-9]+", line)
+            value = valueList[0]
+            total += value
+            i += 1
+            
+print(total / i)
